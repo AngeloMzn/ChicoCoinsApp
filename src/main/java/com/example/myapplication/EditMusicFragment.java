15a43c5e025177d65b5aca724555f66996441a7f
adrian-dutra <117208341+adrian-dutra@users.noreply.github.com>
2024-04-08 18:03:13 -0400

    [FEAT] Adicionando navegabilidade no botão do fragmento music,

--- src/main/java/com/example/myapplication/EditMusicFragment.java
+++ src/main/java/com/example/myapplication/EditMusicFragment.java
@@ -1,9 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +16,7 @@ import com.example.myapplication.databinding.MusicFragmentBinding;
 public class EditMusicFragment extends Fragment {
 
     private MusicFragmentBinding binding;
+    private Button btn_BackHistory;
 
     @Override
     public View onCreateView(
@@ -22,14 +25,26 @@ public class EditMusicFragment extends Fragment {
     ) {
 
         binding = MusicFragmentBinding.inflate(inflater, container, false);
-        return binding.getRoot();
 
+        View view = binding.getRoot();
+
+        Button btn_BackHistory = view.findViewById(R.id.btn_BackHistory);
+
+        btn_BackHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent itBackHistory = new Intent(getActivity(), ChicoHistoryActivity.class);
+                startActivity(itBackHistory);
+            }
+        });
+        return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
     }
+
 
     @Override
     public void onDestroyView() {
