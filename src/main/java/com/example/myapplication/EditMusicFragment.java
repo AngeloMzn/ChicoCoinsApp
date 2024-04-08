d6f7d307d96dc8928caef6b55efb2342faf9a4e0
Ângelo Vinicius Hernandez Mazarin <angelotrab22@gmail.com>
2024-04-08 17:29:27 -0300

    [FIX] Corrigindo metodos e arquivos novamente

--- src/main/java/com/example/myapplication/EditMusicFragment.java
+++ src/main/java/com/example/myapplication/EditMusicFragment.java
@@ -7,13 +7,13 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.myapplication.databinding.FragmentFirstBinding;
 
-public class FirstFragment extends Fragment {
+import com.example.myapplication.databinding.MusicFragmentBinding;
 
-    private FragmentFirstBinding binding;
+public class EditMusicFragment extends Fragment {
+
+    private MusicFragmentBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +21,7 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding = MusicFragmentBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -29,10 +29,6 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(v ->
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
-        );
     }
 
     @Override
